676a3366f8d215bb35d109562874be549d0275c0
darkwood <tore.morkved@gmail.com>
2018-08-12 23:00:47 +0200

    app center setup

--- TestPush/App.xaml.cs
+++ TestPush/App.xaml.cs
@@ -1,6 +1,9 @@
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace TestPush
@@ -17,6 +20,7 @@ namespace TestPush
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("ios=77a9f213-3259-4e42-9712-4992acd109d4;" + "uwp={Your UWP App secret here};" + "android={Your Android App secret here}", typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
